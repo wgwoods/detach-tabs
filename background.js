72c090c57c011be41c67e822dbfb36ffb31069b1
Will Woods <wwoods@redhat.com>
2020-05-18 11:52:43 -0400

    Fix gratuitous window creation, refactor sort/tab-detach functions
    * Refactor `detach_tabs()` and `byIndex()` sort into their own functions
    * Add some `console.debug()` calls for debugging
    * Don't detach tabs if it would move all tabs to a new window

--- background.js
+++ background.js
@@ -19,39 +19,49 @@ browser.contextMenus.create({
     onclick: detach_tabs_same_origin_across_windows
 });
 
-async function detach_tabs_to_the_right(_, tab) {
-    const current_tabs = await browser.tabs.query({ currentWindow: true });
-    const ids_to_move = current_tabs
-        .sort((a, b) => a.index - b.index)
-        .slice(tab.index + 1)
-        .map(tab => tab.id);
+function byIndex(a, b) {
+    return a.index - b.index;
+}
 
-    const window = await browser.windows.create({ tabId: tab.id });
-    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+async function detach_tabs(tabs) {
+    console.debug(`detach_tabs: moving ${tabs.length} tab${tabs.length > 1 ? 's' : ''}`);
+    const tab1 = tabs.shift();
+    const window = await browser.windows.create({ tabId: tab1.id });
+    await browser.tabs.move(tabs.map(t => t.id), { windowId: window.id, index: 1 });
+}
+
+async function detach_tabs_to_the_right(_, tab) {
+    // If there aren't tabs to our left, we've got nothing to do
+    if (tab.index == 0) {
+        console.debug("detach_tabs_to_the_right: not moving all tabs");
+        return;
+    }
+    const current_tabs = await browser.tabs.query({ currentWindow: true });
+    await detach_tabs(current_tabs.sort(byIndex).slice(tab.index));
 }
 
 async function detach_tabs_same_origin(_, tab) {
     const url = new URL(tab.url);
     const tab_origin = `${url.origin}/*`;
-
-    const current_tabs = await browser.tabs.query({ currentWindow: true, url: tab_origin });
-    const ids_to_move = current_tabs
-        .sort((a, b) => a.index - b.index)
-        .map(tab => tab.id);
-
-    const window = await browser.windows.create({ tabId: tab.id });
-    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+    const current_tabs = await browser.tabs.query({ currentWindow: true });
+    const origin_tabs = await browser.tabs.query({ currentWindow: true, url: tab_origin });
+    // If the current window is already tabs from the same origin, we've got nothing to do
+    if (current_tabs.length == origin_tabs.length) {
+        console.debug("detach_tabs_same_origin: not moving all tabs");
+        return;
+    }
+    await detach_tabs(origin_tabs.sort(byIndex));
 }
 
 async function detach_tabs_same_origin_across_windows(_, tab) {
     const url = new URL(tab.url);
     const tab_origin = `${url.origin}/*`;
-
-    const current_tabs = await browser.tabs.query({ url: tab_origin });
-    const ids_to_move = current_tabs
-        .sort((a, b) => a.index - b.index)
-        .map(tab => tab.id);
-
-    const window = await browser.windows.create({ tabId: tab.id });
-    await browser.tabs.move(ids_to_move, { windowId: window.id, index: 1 });
+    const current_tabs = await browser.tabs.query({ currentWindow: true });
+    const origin_tabs = await browser.tabs.query({ url: tab_origin });
+    // If all the same-origin tabs are already here, we've got nothing to do
+    if (origin_tabs.every(t => t.windowId == tab.windowId)) {
+        console.debug("detach_tabs_same_origin_across_windows: not moving all tabs");
+        return;
+    }
+    await detach_tabs(origin_tabs.sort(byIndex));
 }
