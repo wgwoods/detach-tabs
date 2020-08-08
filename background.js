d059e455696c4bf56c8b543e7c5cc80abebbaf36
Will Woods <wwoods@redhat.com>
2020-08-08 16:46:31 -0400

    Simplified/renamed/forked 'Poptabs' extension
    * Renamed to 'Poptabs'
    * Dropped "from-origin" stuff so no extra perms required
    * Added keyboard shortcuts
    * This is basically a different extension now

--- background.js
+++ background.js
@@ -1,97 +1,68 @@
-browser.contextMenus.create({
-    id: "detach-tabs-to-the-right",
-    title: browser.i18n.getMessage("detach-to-the-right"),
+/* Make context menu items */
+browser.menus.create({
+    id: "pop-this-tab",
+    title: browser.i18n.getMessage("pop-this-tab"),
     contexts: ["tab"],
-    onclick: detach_tabs_to_the_right
+    onclick: async (info, tab) => { pop_this_tab(tab) }
 });
 
-browser.contextMenus.create({
-    id: "detach-tabs-same-origin",
-    title: browser.i18n.getMessage("detach-same-origin"),
+browser.menus.create({
+    id: "pop-tabs-to-the-right",
+    title: browser.i18n.getMessage("pop-tabs-to-the-right"),
     contexts: ["tab"],
-    onclick: detach_tabs_same_origin
+    onclick: async (info, tab) => { pop_tabs_to_the_right(tab) }
 });
 
-browser.contextMenus.create({
-    id: "detach-tabs-same-origin-across-windows",
-    title: browser.i18n.getMessage("detach-same-origin-across-windows"),
-    contexts: ["tab"],
-    onclick: detach_tabs_same_origin_across_windows
+/* Disable menu items when they aren't valid */
+browser.menus.onShown.addListener(async (info, tab) => {
+    // TODO: do we need to check for hidden tabs?
+    const current_tabs = await browser.tabs.query({ currentWindow: true });
+    browser.menus.update("pop-this-tab",          { enabled: (current_tabs.length > 1) });
+    browser.menus.update("pop-tabs-to-the-right", { enabled: (tab.index > 0) });
+    browser.menus.refresh();
 });
 
+/* Key command listener is defined in manifest.json */
 browser.commands.onCommand.addListener(async (command) => {
     const active_tabs = await browser.tabs.query({ currentWindow: true, active: true });
     const tab = active_tabs[0];
-    if (command === "detach-to-the-right") {
-        detach_tabs_to_the_right(undefined, tab);
-    } else if (command === "detach-same-origin") {
-        detach_tabs_same_origin(undefined, tab);
-    } else if (command === "detach-same-origin-across-windows") {
-        detach_tabs_same_origin_across_windows(undefined, tab);
-    } else if (command === "detach-this-tab") {
-        detach_this_tab(tab);
+    if (command === "pop-this-tab") {
+        pop_this_tab(tab);
+    } else if (command === "pop-tabs-to-the-right") {
+        pop_tabs_to_the_right(tab);
     } else {
-        console.error(`detach-tabs: unknown command '${command}'`);
+        console.error(`poptabs: unknown command '${command}'`);
     }
 });
 
-browser.contextMenus.onShown.addListener((info, tab) => {
-    browser.contextMenus.update("detach-tabs-to-the-right", {enabled: (tab.index > 0)});
-    browser.contextMenus.refresh();
-});
+/* Sorting helper */
+function byIndex(a, b) { return a.index - b.index; }
 
-function byIndex(a, b) {
-    return a.index - b.index;
-}
-
-async function detach_tabs(tabs) {
-    console.debug(`detach_tabs: moving ${tabs.length} tab${tabs.length > 1 ? 's' : ''}`);
+/* Actually move the given array of tabs to a new window */
+async function _pop_tabs(tabs) {
+    console.debug(`_pop_tabs: moving ${tabs.length} tab${tabs.length > 1 ? 's' : ''}`);
     const tab1 = tabs.shift();
     const window = await browser.windows.create({ tabId: tab1.id });
     await browser.tabs.move(tabs.map(t => t.id), { windowId: window.id, index: 1 });
 }
 
-async function detach_this_tab(tab) {
+async function pop_this_tab(tab) {
+    // If this is the only tab, there's nothing to do
     const current_tabs = await browser.tabs.query({ currentWindow: true });
     if (current_tabs.length == 1) {
-        console.debug("detach_this_tab: this is the only tab; not moving");
+        console.debug("pop_this_tab: this is the only tab; not moving");
         return;
     }
-    await detach_tabs([tab]);
+    await _pop_tabs([tab]);
 }
 
-async function detach_tabs_to_the_right(_, tab) {
+async function pop_tabs_to_the_right(tab) {
     // If there aren't tabs to our left, we've got nothing to do
     if (tab.index == 0) {
-        console.debug("detach_tabs_to_the_right: not moving all tabs");
+        console.debug("pop_tabs_to_the_right: not moving all tabs");
         return;
     }
     const current_tabs = await browser.tabs.query({ currentWindow: true });
-    await detach_tabs(current_tabs.sort(byIndex).slice(tab.index));
+    await _pop_tabs(current_tabs.sort(byIndex).slice(tab.index));
 }
 
-async function detach_tabs_same_origin(_, tab) {
-    const url = new URL(tab.url);
-    const tab_origin = `${url.origin}/*`;
-    const current_tabs = await browser.tabs.query({ currentWindow: true });
-    const origin_tabs = await browser.tabs.query({ currentWindow: true, url: tab_origin });
-    // If the current window is already tabs from the same origin, we've got nothing to do
-    if (current_tabs.length == origin_tabs.length) {
-        console.debug("detach_tabs_same_origin: not moving all tabs");
-        return;
-    }
-    await detach_tabs(origin_tabs.sort(byIndex));
-}
-
-async function detach_tabs_same_origin_across_windows(_, tab) {
-    const url = new URL(tab.url);
-    const tab_origin = `${url.origin}/*`;
-    const current_tabs = await browser.tabs.query({ currentWindow: true });
-    const origin_tabs = await browser.tabs.query({ url: tab_origin });
-    // If all the same-origin tabs are already here, we've got nothing to do
-    if (origin_tabs.every(t => t.windowId == tab.windowId)) {
-        console.debug("detach_tabs_same_origin_across_windows: not moving all tabs");
-        return;
-    }
-    await detach_tabs(origin_tabs.sort(byIndex));
-}
